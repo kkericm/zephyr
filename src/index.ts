5a5840e3cd52f4c9fda6144a3e18a55fa04d8793
kkeric <ericmelo2765@gmail.com>
2024-03-03 15:08:20 -0300

    Commit #003

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import TelegramBot from 'node-telegram-bot-api';
-import path from "path"
-import fs from "fs"
-import qr from "qrcode"
-import ytdl from "ytdl-core"
-import ytSeach from "yt-search"
+import { config } from "dotenv"; config();
+import path from "path";
+import fs from "fs";
+import qr from "qrcode";
+import ytdl from "ytdl-core";
+import ytSeach from "yt-search";
+import Jimp from "jimp";
 
-const bot = new TelegramBot("6157411466:AAGUFR2hh1aknQinOZO21i3S1WF3eqTUafM", { polling: true });
+const bot = new TelegramBot(process.env.TOLKEN as string, { polling: true });
 
-var config = {
+var settings = {
     waitParam: false,
     commandRes: '',
     callOn: false
@@ -19,71 +21,187 @@ bot.setMyCommands([
 ])
 
 
-function commands(event: TelegramBot.Message, param: string[]): { [key: string]: any } {
+function privateCommands(event: TelegramBot.Message, param: string[]): { [key: string]: any } {
     return {
         start() {
             bot.sendMessage(event.chat.id, `Olá`)
         },
         qrcode() {
             if (param.length === 0) {
-                bot.sendMessage(event.chat.id, "Foneça o link...")
-                config.waitParam = true
-                config.commandRes = "qrcode"
+                bot.sendMessage(event.chat.id, "Foneça o conteudo do código QR.")
+                settings.waitParam = true
+                settings.commandRes = "qrcode"
             } else {
+                bot.sendMessage(event.chat.id, "Enviando, aguarde...")
                 qr.toBuffer(param[0], (err, buff) => {
                     bot.sendPhoto(event.chat.id, buff)
                 })
             }
         },
         async yt() {
-            const vid = (await ytSeach({query: param[0], category: "music"})).videos[0]
-            let th = await bot.sendMessage(event.chat.id as number, "Enviando, aguarde...")
-            try {
-                let videoInfo = await ytdl.getInfo(vid.url);
-                let audioFormat = ytdl.chooseFormat(videoInfo.formats, { filter: 'audioonly' });
-                const audioStream = ytdl.downloadFromInfo(videoInfo, {
-                    format: audioFormat,
-                    quality: 'highestaudio',
-                });
-                let audioBuffer = Buffer.from('');
-                audioStream.on('data', (chunk) => {
-                    audioBuffer = Buffer.concat([audioBuffer, chunk]);
-                });
-                audioStream.on('end', () => {
-                    bot.sendAudio(event.chat.id as number, audioBuffer, {
-                        title: videoInfo.videoDetails.title + '.mp3', 
-                        performer: videoInfo.videoDetails.author.name
-                    }).then(r => {
-                        bot.deleteMessage(th.chat.id, th.message_id)
+            if (param[0] !== undefined) {
+                var vid = (await ytSeach({query: param[0], category: "music"})).videos[0]
+                let th = await bot.sendMessage(event.chat.id as number, "Enviando, aguarde...")
+                try {
+                    if (vid === undefined) {
+                        bot.sendMessage(event.chat.id, "Houve um erro.")
+                    } else {
+                        let videoInfo: ytdl.videoInfo = await ytdl.getInfo(vid.url);
+                        let audioFormat = ytdl.chooseFormat(videoInfo.formats, { filter: 'audioonly' });
+                        const audioStream = ytdl.downloadFromInfo(videoInfo, {
+                        format: audioFormat,
+                        quality: 'highestaudio',
                     });
-                });
+                    let audioBuffer = Buffer.from('');
+                    audioStream.on('data', (chunk) => {
+                        audioBuffer = Buffer.concat([audioBuffer, chunk]);
+                    });
+                    audioStream.on('end', () => {
+                        bot.sendAudio(event.chat.id as number, audioBuffer, {
+                            title: videoInfo.videoDetails.title + '.mp3', 
+                            performer: videoInfo.videoDetails.author.name
+                        }).then(r => {
+                            bot.deleteMessage(th.chat.id, th.message_id)
+                        });
+                    });
+                }
             } catch (error) {
                 bot.sendMessage(event.chat.id, "Houve um erro.")
-                console.error(error);
+                // console.error(error);
                 throw error;
             }
-        }
+            } else {
+                bot.sendMessage(event.chat.id, "Qual o nome da música?")
+                settings.waitParam = true
+                settings.commandRes = "yt"
+            }
+        },
     }
 }
 
-function waits(event: TelegramBot.Message): { [key: string]: any } {
+function privateWaits(event: TelegramBot.Message): { [key: string]: any } {
     return {
-        // what(...params: any[]) {
-        //     bot.sendMessage(event.chat.id, `Ah sim ${params}, entendi...`)
-        //     config.commandRes = ''
-        //     config.waitParam = false
-        // },
         qrcode(...params: any[]) {
+            bot.sendMessage(event.chat.id, "Enviando, aguarde...");
             qr.toBuffer(params[0], (err, buff) => {
-                bot.sendPhoto(event.chat.id, buff)
-            })
-            config.commandRes = ''
-            config.waitParam = false
+                bot.sendPhoto(event.chat.id, buff);
+            });
+            settings.commandRes = '';
+            settings.waitParam = false;
+        },
+        yt() {
+            privateCommands(event, [event.text as string]).yt();
+            settings.commandRes = '';
+            settings.waitParam = false;
         }
     }
 }
 
-function calls(event: TelegramBot.CallbackQuery, data: string[]): { [key: string]: any } {
+function privateCalls(event: TelegramBot.CallbackQuery, data: string[]): { [key: string]: any } {
+    return {
+        hell() {
+            bot.editMessageText(`Você escolheu ${data[0] === "yes" ? "sim": "não"}.`, {
+                chat_id: event.message?.chat.id,
+                message_id: event.message?.message_id
+            })
+        }
+    }
+}
+
+
+function groupCommands(event: TelegramBot.Message, param: string[]): { [key: string]: any } {
+    return {
+        start() {
+            bot.sendMessage(event.chat.id, `Olá`)
+        },
+        qrcode() {
+            if (param.length === 0) {
+                bot.sendMessage(event.chat.id, "Foneça o conteudo do código QR.", {
+                    reply_to_message_id: event.message_id
+                })
+                settings.waitParam = true
+                settings.commandRes = "qrcode"
+            } else {
+                bot.sendMessage(event.chat.id, "Enviando, aguarde...", {
+                    reply_to_message_id: event.message_id
+                })
+                qr.toBuffer(param[0], (err, buff) => {
+                    bot.sendPhoto(event.chat.id, buff)
+                })
+            }
+        },
+        async yt() {
+            if (param[0] !== undefined) {
+                var vid = (await ytSeach({query: param[0], category: "music"})).videos[0]
+                let th = await bot.sendMessage(event.chat.id as number, "Enviando, aguarde...", {
+                    reply_to_message_id: event.message_id
+                })
+                try {
+                    if (vid === undefined) {
+                        bot.sendMessage(event.chat.id, "Houve um erro.", {
+                            reply_to_message_id: event.message_id
+                        })
+                    } else {
+                        let videoInfo: ytdl.videoInfo = await ytdl.getInfo(vid.url);
+                        let audioFormat = ytdl.chooseFormat(videoInfo.formats, { filter: 'audioonly' });
+                        const audioStream = ytdl.downloadFromInfo(videoInfo, {
+                        format: audioFormat,
+                        quality: 'highestaudio',
+                    });
+                    let audioBuffer = Buffer.from('');
+                    audioStream.on('data', (chunk) => {
+                        audioBuffer = Buffer.concat([audioBuffer, chunk]);
+                    });
+                    audioStream.on('end', () => {
+                        bot.sendAudio(event.chat.id as number, audioBuffer, {
+                            title: videoInfo.videoDetails.title + '.mp3', 
+                            performer: videoInfo.videoDetails.author.name
+                        }).then(r => {
+                            bot.deleteMessage(th.chat.id, th.message_id)
+                        });
+                    });
+                }
+            } catch (error) {
+                bot.sendMessage(event.chat.id, "Houve um erro.", {
+                    reply_to_message_id: event.message_id
+                })
+                // console.error(error);
+                throw error;
+            }
+            } else {
+                bot.sendMessage(event.chat.id, "Qual o nome da música?", {
+                    reply_to_message_id: event.message_id
+                })
+                settings.waitParam = true
+                settings.commandRes = "yt"
+            }
+        },
+    }
+}
+
+function groupWaits(event: TelegramBot.Message): { [key: string]: any } {
+    return {
+        qrcode(...params: any[]) {
+            bot.sendMessage(event.chat.id, "Enviando, aguarde...", {
+                reply_to_message_id: event.message_id
+            });
+            qr.toBuffer(params[0], (err, buff) => {
+                bot.sendPhoto(event.chat.id, buff, {
+                    reply_to_message_id: event.message_id
+                });
+            });
+            settings.commandRes = '';
+            settings.waitParam = false;
+        },
+        yt() {
+            groupCommands(event, [event.text as string]).yt();
+            settings.commandRes = '';
+            settings.waitParam = false;
+        }
+    }
+}
+
+function groupCalls(event: TelegramBot.CallbackQuery, data: string[]): { [key: string]: any } {
     return {
         hell() {
             bot.editMessageText(`Você escolheu ${data[0] === "yes" ? "sim": "não"}.`, {
@@ -96,24 +214,38 @@ function calls(event: TelegramBot.CallbackQuery, data: string[]): { [key: string
 
 bot.on("message", event => {
     let msg = event.text as string;
-    if (msg.startsWith("/")) {
-        config.waitParam = false;
-        config.commandRes = '';
+    if (msg.startsWith("/") && event.chat.type === "group") {
+        settings.waitParam = false;
+        settings.commandRes = '';
         let command = msg.slice(1).split(" ");
-        let _commands = commands(event, command.slice(1));
-        if (Object.keys(_commands).includes(command[0])) _commands[command[0]]();
-        else bot.sendMessage(event.chat.id, "Comando desconhecido.");
-
-    } else if (config.waitParam) {
-        waits(event)[config.commandRes](msg);
+        let _commands = groupCommands(event, command.slice(1));
+        if (Object.keys(_commands).includes(command[0])) bot.sendChatAction(event.chat.id, "typing").then(r => {
+            _commands[command[0]]();
+        });
+    } else if (settings.waitParam && event.chat.type === "group") {
+        bot.sendChatAction(event.chat.id, "typing").then(r => {
+            groupWaits(event)[settings.commandRes](msg);
+        })
+    } else if (msg.startsWith("/") && event.chat.type === "private") {
+        settings.waitParam = false;
+        settings.commandRes = '';
+        let command = msg.slice(1).split(" ");
+        let _commands = privateCommands(event, command.slice(1));
+        if (Object.keys(_commands).includes(command[0])) bot.sendChatAction(event.chat.id, "typing").then(r => {
+            _commands[command[0]]();
+        });
+    } else if (settings.waitParam && event.chat.type === "private") {
+        bot.sendChatAction(event.chat.id, "typing").then(r => {
+            privateWaits(event)[settings.commandRes](msg);
+        })
     }
 })
 
 bot.on("callback_query", event => {
     let dal = event.data?.split(":") as string[];
-    if (!config.callOn) {
-        config.callOn = true;
-        calls(event, dal.slice(1))[dal[0]]();
+    if (!settings.callOn) {
+        settings.callOn = true;
+        privateCalls(event, dal.slice(1))[dal[0]]();
     }
 })
 
